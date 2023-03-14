211b857c7a52ecacb9cebdf5faf54617a98c992d
cindyjeby <cindyjeby@gmail.com>
2023-03-14 05:07:03 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *_strdup - duplicate to new memory space location
+ *@str: char
+ *Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *a;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	a = malloc(strlen(str) + 1);
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(a, str);
+	return (a);
+}
