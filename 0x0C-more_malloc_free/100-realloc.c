9df8b88a783f6cc54d47c9a262c5ad98225de255
cindyjeby <cindyjeby@gmail.com>
2023-03-16 10:13:04 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,7 +19,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		p = malloc(new_size);
 		return (p);
 	}
-	else if (new_size == NULL)
+	else if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
@@ -31,7 +31,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (p == NULL)
 		return (NULL);
 	if (new_size > old_size)
-		n = oldsize;
+		n = old_size;
 	for (i = 0; i < n; i++)
 		p[i] = oldp[i];
 	free(ptr);
