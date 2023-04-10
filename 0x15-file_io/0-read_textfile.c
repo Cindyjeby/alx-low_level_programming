857141e694531f42a3bf7fded6df777f36289606
cindyjeby <cindyjeby@gmail.com>
2023-04-10 06:45:09 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * read_textfile - reads a text file and prints it the posix standard output
+ * @letters: number of letters it should print and read
+ * @filename: file being read
+ * Return: actual number of letters it could read, 0 if file can not be opened, read or filename is NULL
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *l;
+	ssize_t ch;
+	ssize_t k;
+	ssize_t t;
+
+	ch = fopen(filename, O_RDONLY);
+
+	if (ch == -1)
+		return (0);
+
+	l = malloc(sizeof(char) *letters);
+	t = read(ch, l, letters);
+	k = write(STDOUT_FILENO, l, t);
+
+	free(l);
+	fclose(ch);
+	return (k);
+}
