1b465ed190302912b5f55f6cce3170e2c038ea03
cindyjeby <cindyjeby@gmail.com>
2023-06-15 06:54:06 -0700

    new node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,9 +7,10 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *new;
+	new = malloc(sizeof(dlistint_t));
 
-	if (!new)
+	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->prev = NULL;
